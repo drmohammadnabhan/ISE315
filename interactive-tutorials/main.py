2bac05059b15a598d7af0e01f1bf319152cb7cf5
drmohammadnabhan <drmohammadnabhan@gmail.com>
2025-01-22 19:43:52 +0300

    Update main.py

--- interactive-tutorials/main.py
+++ interactive-tutorials/main.py
@@ -1,12 +1,9 @@
 import streamlit as st
 
-st.set_page_config(page_title="ISE-315 Tutorials", layout="wide")
+st.set_page_config(page_title="ISE315 - Interactive Tutorials", layout="wide")
 
-st.title("Welcome to ISE-315 Interactive Tutorials")
-st.markdown("""
-### Available Tutorials
-- Navigate to the tutorials using the sidebar.
-""")
+st.title("Welcome to ISE315 Interactive Tutorials")
+st.markdown("### Navigate to different tutorials from the sidebar.")
 
 st.sidebar.title("Navigation")
-st.sidebar.info("Use the sidebar to navigate through different tutorials.")
+st.sidebar.page_link("pages/type1_type2_properties.py", label="Type I and II Error Properties")
